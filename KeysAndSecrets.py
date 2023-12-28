a7c8f49dd345a0bb8eb1b9ea830d0f99bcd8d6d3
John Hofmann <johnphofmann33@gmail.com>
2023-12-27 19:29:11 -0600

    initial commit

---
+++ KeysAndSecrets.py
@@ -0,0 +1,8 @@
+'''
+This file is designated to hold all secrets and keys needed for any part of this project. 
+Updates here will be minimal as it will mostly be gitignored unless going back and formatting documentation.
+
+'''
+
+
+
